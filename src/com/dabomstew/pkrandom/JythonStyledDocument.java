ac5912d0de76a4cc09b1dfa4ae1c3e6160ae93a4
TomSemeijn <tomsemeijn98@gmail.com>
2022-07-02 18:06:34 +0200

    Add script highlighting for strings and function names

--- src/com/dabomstew/pkrandom/JythonStyledDocument.java
+++ src/com/dabomstew/pkrandom/JythonStyledDocument.java
@@ -13,6 +13,10 @@ public class JythonStyledDocument extends DefaultStyledDocument {
     private Style commentStyle;
     private Style keywordStyle;
 
+    private Style funcStyle;
+
+    private Style stringStyle;
+
     private static String[] keywords = {
             "def", "import", "from", "return"
     };
@@ -26,6 +30,10 @@ public class JythonStyledDocument extends DefaultStyledDocument {
         keywordStyle = styleContext.addStyle("keyword", null);
         StyleConstants.setForeground(keywordStyle, Color.blue);
         StyleConstants.setBold(keywordStyle, true);
+        funcStyle = styleContext.addStyle("function", null);
+        StyleConstants.setForeground(funcStyle, new Color(178, 60, 178));
+        stringStyle = styleContext.addStyle("string", null);
+        StyleConstants.setForeground(stringStyle, new Color(211, 144, 116));
     }
 
     public void insertString (int offset, String str, AttributeSet a) throws BadLocationException {
@@ -59,10 +67,22 @@ public class JythonStyledDocument extends DefaultStyledDocument {
             setCharacterAttributes(p0, word._word.length(), keywordStyle, true);
         }
 
+        final List<HiliteWord> functions = findFunctions(text);
+        for(HiliteWord word : functions) {
+            int p0 = word._position;
+            setCharacterAttributes(p0, word._word.length(), funcStyle, true);
+        }
+
         final List<HiliteWord> comments = findComments(text);
         for(HiliteWord word : comments) {
             int p0 = word._position;
             setCharacterAttributes(p0, word._word.length(), commentStyle, true);
+        }
+
+        final List<HiliteWord> strings = findStrings(text);
+        for(HiliteWord word : strings) {
+            int p0 = word._position;
+            setCharacterAttributes(p0, word._word.length(), stringStyle, true);
         }
     }
 
@@ -112,6 +132,94 @@ public class JythonStyledDocument extends DefaultStyledDocument {
         }
 
         return hiliteWords;
+    }
+
+    private static List<HiliteWord> findFunctions(String content)
+    {
+        content += " ";
+        List<HiliteWord> hiliteWords = new ArrayList<HiliteWord>();
+        int lastWhitespacePosition = 0;
+        String word = "";
+        HiliteWord lastWord = null;
+        char[] data = content.toCharArray();
+
+        for(int index=0; index < data.length; index++) {
+            char ch = data[index];
+            if(!(Character.isLetter(ch) || Character.isDigit(ch) || ch == '_')) {
+                lastWhitespacePosition = index;
+                if(word.length() > 0) {
+                    lastWord = new HiliteWord(word, lastWhitespacePosition - word.length());
+                    word="";
+                }
+            }
+            if(ch == '(')
+            {
+                if(lastWord != null)
+                {
+                    hiliteWords.add(lastWord);
+                    lastWord = null;
+                }
+            }
+            else {
+                word += ch;
+                lastWord = null;
+            }
+        }
+        return hiliteWords;
+    }
+
+    private static List<HiliteWord> findStrings(String content)
+    {
+        content += " ";
+        List<HiliteWord> found = new ArrayList<HiliteWord>();
+        int lastQuote = -1;
+        char quoteType = '\0';
+        boolean escaped = false;
+        boolean inComment = false;
+        char[] data = content.toCharArray();
+
+        for(int index=0; index < data.length; index++) {
+            char ch = data[index];
+            switch(ch)
+            {
+                case '\\':
+                    escaped = true;
+                    break;
+                case '#':
+                    inComment = true;
+                    break;
+                case '\n':
+                    inComment = false;
+                    break;
+                case '"':
+                case '\'':
+                    if(!escaped) {
+                        if (lastQuote == -1) {
+                            if(!inComment)
+                            {
+                                lastQuote = index;
+                                quoteType = ch;
+                            }
+                        }
+                        else if(quoteType == ch)
+                        {
+                            String str = new String(data, lastQuote, (index + 1) - lastQuote);
+                            found.add(new HiliteWord(str, lastQuote));
+                            lastQuote = -1;
+                            quoteType = '\0';
+                        }
+                    }
+                    break;
+            }
+            if(escaped && ch != '\\'){ escaped = false; }
+        }
+        if(lastQuote != -1)
+        {
+            String str = new String(data, lastQuote, data.length - 1 - lastQuote);
+            found.add(new HiliteWord(str, lastQuote));
+        }
+
+        return found;
     }
 
     private static final boolean isKeyword(String word) {
