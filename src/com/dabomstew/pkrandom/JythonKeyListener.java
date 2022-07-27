b2f0d3d0f189653e966a311571763196bc6123b7
TomSemeijn <tomsemeijn98@gmail.com>
2022-07-27 15:38:44 +0200

    Add advanced line manipulation features to the script input
    when no text is selected:
       ctrl->d will duplicate an entire line
       ctrl->c will copy an entire line
       ctrl->x will cut an entire line
       ctrl->v will paste an entire line before the line the caret is in (if a single full line was copied)

--- src/com/dabomstew/pkrandom/JythonKeyListener.java
+++ src/com/dabomstew/pkrandom/JythonKeyListener.java
@@ -1,8 +1,15 @@
 package com.dabomstew.pkrandom;
 
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.IOException;
 
 public class JythonKeyListener implements KeyListener {
 
@@ -15,6 +22,8 @@ public class JythonKeyListener implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e){
+
+        //home key accounting for leading tabs
         if(e.getKeyCode() == KeyEvent.VK_HOME){
 
             String text = editor.getText();
@@ -50,6 +59,83 @@ public class JythonKeyListener implements KeyListener {
 
             //consume event
             e.consume();
+        }
+        //copying, pasting, and cutting full lines when there is no selection
+        else if(editor.getSelectedText() == null && (e.getKeyCode() == KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_X || e.getKeyCode() == KeyEvent.VK_V || e.getKeyCode() == KeyEvent.VK_C) && (e.getModifiersEx() & KeyEvent.CTRL_DOWN_MASK) != 0)
+        {
+            final int caretPos = editor.getCaretPosition();
+            final String text = editor.getText();
+            if(text.length() == 0){ return; }
+
+            int lineStart = caretPos;
+            while(lineStart > 0 && text.charAt(lineStart - 1) != '\n')
+            {
+                lineStart--;
+            }
+            int lineEnd = lineStart + 1;
+            while(lineEnd < text.length() && text.charAt(lineEnd - 1) != '\n')
+            {
+                lineEnd++;
+            }
+            String line = text.substring(lineStart, lineEnd);
+            if(!line.endsWith("\n")){ line += "\n"; }
+
+            //if ctr->x
+            if(e.getKeyCode() == KeyEvent.VK_X)
+            {
+                //remove line from editor
+                try{
+                    editor.getDocument().remove(lineStart, lineEnd - lineStart);
+                }
+                catch(BadLocationException exception)
+                {}
+
+                //copy line to user clipboard (from https://stackoverflow.com/questions/3591945/copying-to-the-clipboard-in-java)
+                StringSelection selection = new StringSelection(line);
+                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                clipboard.setContents(selection, selection);
+
+                e.consume();
+            }
+            //if ctr->c
+            else if(e.getKeyCode() == KeyEvent.VK_C)
+            {
+                //copy line to user clipboard (from https://stackoverflow.com/questions/3591945/copying-to-the-clipboard-in-java)
+                StringSelection selection = new StringSelection(line);
+                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                clipboard.setContents(selection, selection);
+
+                e.consume();
+            }
+            //if ctr->v
+            else if(e.getKeyCode() == KeyEvent.VK_V)
+            {
+                //if text to paste is a single full line
+                try{
+                    String data = (String) Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor);
+                    int newlineCount = 0;
+                    for(int k = 0; k < data.length(); k++) { if(data.charAt(k) == '\n'){ newlineCount++; } }
+                    if(newlineCount == 1 && data.endsWith("\n"))
+                    {
+                        try{
+                            editor.getDocument().insertString(lineStart, data, null);
+                        }
+                        catch(BadLocationException exception){}
+                        e.consume();
+                    }
+                }
+                catch(UnsupportedFlavorException exception){}
+                catch(IOException exception){}
+            }
+            //if ctrl->d
+            else if(e.getKeyCode() == KeyEvent.VK_D)
+            {
+                try{
+                    editor.getDocument().insertString(lineStart, line, null);
+                }
+                catch(BadLocationException exception){}
+                e.consume();
+            }
         }
     }
 
