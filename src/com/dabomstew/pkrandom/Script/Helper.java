ddeaaccc0b3d3cd36eaa7540eca128883492786a
TomSemeijn <tomsemeijn98@gmail.com>
2022-10-06 16:19:57 +0200

    Add: helper functions for converting enum and index values to strings for better script debugging

--- src/com/dabomstew/pkrandom/Script/Helper.java
+++ src/com/dabomstew/pkrandom/Script/Helper.java
@@ -1,9 +1,14 @@
 package com.dabomstew.pkrandom.Script;
 
+import com.dabomstew.pkrandom.constants.Abilities;
+import com.dabomstew.pkrandom.constants.Items;
+import com.dabomstew.pkrandom.constants.Moves;
+import com.dabomstew.pkrandom.constants.Species;
 import com.dabomstew.pkrandom.pokemon.Pokemon;
 import com.dabomstew.pkrandom.pokemon.Type;
 import org.python.core.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class Helper {
@@ -40,6 +45,48 @@ public class Helper {
     public static boolean HasType(Pokemon poke, Type theType)
     {
         return poke.primaryType == theType || poke.secondaryType == theType;
+    }
+
+    public static PyString Str(Enum val)
+    {
+        return new PyString(val.name());
+    }
+
+    public enum Index{
+        ABILITY, ITEM, MOVE, POKEMON
+    }
+    public static PyString Str(int val, Index valType)throws IllegalAccessException, Exception
+    {
+        Field[] fields = null;
+        switch(valType)
+        {
+            case ABILITY:
+                if(val == Abilities.staticTheAbilityNotTheKeyword){ return new PyString("static"); } //funny edge case
+                fields = Abilities.class.getFields();
+                break;
+            case ITEM:
+                fields = Items.class.getFields();
+                break;
+            case MOVE:
+                fields = Moves.class.getFields();
+                break;
+            case POKEMON:
+                fields = Species.class.getFields();
+                break;
+            default:
+                throw new Exception("Unsupported Index type given to Helper.Str(int val, Helper.Index valType)!");
+        }
+
+        for(Field current : fields)
+        {
+            int currentVal = (int)current.get(null);
+            if(currentVal == val)
+            {
+                return new PyString(current.getName());
+            }
+        }
+
+        throw new Exception("Name of "+valType.name()+" not found!");
     }
 
 }
